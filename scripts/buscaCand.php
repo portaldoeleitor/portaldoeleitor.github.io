1b8321748576081e25d69d6e6352ef9eb769e37c
Tiago Floriano <tiagofloriano@gmail.com>
2016-03-09 11:31:50 -0300

    Introdução á construção da página onde mostra os dados do candidato.

--- scripts/buscaCand.php
+++ scripts/buscaCand.php
@@ -16,8 +16,31 @@ if($_POST["a"] == 1){
 
 	$busca = sel($ano."_".$uf."_cand","NOME_CANDIDATO LIKE '%$termo%' or NOME_URNA_CANDIDATO LIKE '%$termo%'");
 	if(total($busca) > 0){
-		$r = fetch($busca);
+		?>
+			<table class="table table-striped table-hover " id="listaCandidatos">
+			  <thead>
+			    <tr>
+			      <th>Nome</th>
+			      <th>Partido</th>
+			      <th>Munic&iacute;pio</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+		<?
+		while($r = fetch($busca)){
+			?>
+			    <tr onclick="abreFicha('<?= $r["id"] ?>','<?= $ano ?>','<?= $uf ?>')">
+			      <td><?= $r["NOME_URNA_CANDIDATO"] ?> <i>(<?= $r["NOME_CANDIDATO"] ?>)</i></td>
+			      <td><?= $r["SIGLA_PARTIDO"] ?></td>
+			      <td><?= $r["DESCRICAO_UE"] ?></td>
+			    </tr>
+			<?
+		}
+		?>
+			  </tbody>
+			</table>
+			<?
 	}else{
-		$bs->msg("Nenhum candidato encontrado.");
+		echo "Nenhum candidato encontrado.";
 	}
 }
--- scripts/fichaCand.php
+++ scripts/fichaCand.php
@@ -10,34 +10,49 @@ conecta();
 
 if($_POST["a"] == 1){
 	$id = str($_POST["id"]);
+	$ano = str($_POST["ano"]);
+	$uf = str($_POST["uf"]);
 
 	$busca = sel($ano."_".$uf."_cand","id = '$id'");
 	if(total($busca) > 0){
+		$r = fetch($busca);
 		?>
-			<table class="table table-striped table-hover " id="listaCandidatos">
-			  <thead>
-			    <tr>
-			      <th>Nome</th>
-			      <th>Partido</th>
-			      <th>Munic&iacute;pio</th>
-			    </tr>
-			  </thead>
-			  <tbody>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="tabbable" id="tabs-865962">
+					<ul class="nav nav-tabs">
+						<li class="active">
+							<a href="#panel-ficha" data-toggle="tab">Ficha</a>
+						</li>
+						<li>
+							<a href="#panel-<?= $ano ?>" data-toggle="tab"><?= $ano ?></a>
+						</li>
+					</ul>
+					<div class="tab-content">
+						<div class="tab-pane active" id="panel-ficha">
+							<div id="numerocand">
+								<?= $r["NUMERO_CANDIDATO"] ?>
+							</div>
+							<div id="nomecand">
+								<b><?= $r["NOME_URNA_CANDIDATO"] ?></b><br>
+								<i>(<?= $r["NOME_CANDIDATO"] ?>)</i>
+							</div>
+							<div style="clear: both"></div>
+							<p>
+								
+							</p>
+						</div>
+						<div class="tab-pane" id="panel-<?= $ano ?>">
+							<p>
+								Howdy, I'm in Section 2.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<?
-		while($r = fetch($busca)){
-			?>
-			    <tr onclick="abreFicha('<?= $r["id"] ?>')">
-			      <td><?= $r["NOME_URNA_CANDIDATO"] ?> <i>(<?= $r["NOME_CANDIDATO"] ?>)</i></td>
-			      <td><?= $r["SIGLA_PARTIDO"] ?></td>
-			      <td><?= $r["DESCRICAO_UE"] ?></td>
-			    </tr>
-			<?
-		}
-		?>
-			  </tbody>
-			</table>
-			<?
 	}else{
-		echo "Nenhum candidato encontrado.";
+		$bs->msg("Nenhum candidato encontrado.");
 	}
 }
